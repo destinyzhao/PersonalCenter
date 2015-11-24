a2cb1be6f97c34bd6aba68610bceb90dae894ab3
zhaojinxiong <joesenzhao@126.com>
2015-11-24 13:55:02 +0800

    V1.0

---
+++ 主页/InformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  InformationViewController.h
+//  主页
+//
+//  Created by Alex on 15/10/20.
+//  Copyright © 2015年 iOS _Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InformationViewController : UIViewController
+
+@end
